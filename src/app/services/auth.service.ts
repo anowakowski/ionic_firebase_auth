b8661c44ee5b064f3f11b5ae8dea1687acf08e53
anowakowski <anowakowski@mailc.net>
2019-12-29 14:49:48 +0100

    added auth services for fb auth, prepare user model

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,10 +9,17 @@ import { FirebaseUIModule } from 'firebaseui-angular';
 export class AuthService {
 
   authStateUser: firebase.User = null;
-  
   constructor(private af: AngularFireAuth) {
     af.authState.subscribe((user) => {
       this.authStateUser = user;
     });
+  }
+
+  get authenticated(): boolean {
+    return this.authStateUser !== null;
+  }
+
+  get currentUser(): firebase.User {
+    return this.authenticated ? this.authStateUser : null;
   }
 }
